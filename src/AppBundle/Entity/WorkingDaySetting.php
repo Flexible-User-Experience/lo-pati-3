3c4716267ccc1bd5f06337f71419d1adef10f2e3
Will <wiglesias83@gmail.com>
2016-06-29 17:59:43 +0200

    add WorkingDaySetting entity

---
+++ src/AppBundle/Entity/WorkingDaySetting.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class WorkingDaySetting
+ * 
+ * @category Entity
+ * @author   Wilson Iglesias <wiglesias83@gmail.com>
+ * @package  AppBundle\Entity
+ * 
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\WorkingDaySettingRepository")
+ */
+class WorkingDaySetting extends AbstractBase
+{
+    
+}
--- src/AppBundle/Repository/WorkingDaySettingRepository.php
+++ src/AppBundle/Repository/WorkingDaySettingRepository.php
@@ -5,12 +5,12 @@ namespace AppBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 /**
- * Class SettingRepository
+ * Class WorkingDaySettingRepository
  *
  * @category Repository
  * @package  AppBundle\Repository
  * @author   Wilson Iglesias <wiglesias83@gmail.com>
  */
-class SettingRepository extends EntityRepository
+class WorkingDaySettingRepository extends EntityRepository
 {
 }
