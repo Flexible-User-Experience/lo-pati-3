b18190b2e52eee2b1b5713a761f28dd663aea6cd
Will <wiglesias83@gmail.com>
2016-06-29 18:13:13 +0200

    improve WorkingDaySetting orm anotation entity

--- src/AppBundle/Entity/WorkingDaySetting.php
+++ src/AppBundle/Entity/WorkingDaySetting.php
@@ -16,5 +16,37 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class WorkingDaySetting extends AbstractBase
 {
-    
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     */
+    private $weekdayNumber;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * @return int
+     */
+    public function getWeekdayNumber()
+    {
+        return $this->weekdayNumber;
+    }
+
+    /**
+     * @param int $weekdayNumber
+     *
+     * @return WorkingDaySetting
+     */
+    public function setWeekdayNumber($weekdayNumber)
+    {
+        $this->weekdayNumber = $weekdayNumber;
+
+        return $this;
+    }
 }
