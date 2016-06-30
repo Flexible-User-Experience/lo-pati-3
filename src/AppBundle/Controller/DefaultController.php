506310f32619f5569ccb10f1a7149a1b558fe948
Wil Iglesias <wiglesias83@gmail.com>
2016-06-30 17:39:35 +0200

    create New Setting controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Setting;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,6 +30,27 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/settings_list.html.twig', array(
             'settingsList' => $settingsList,
+        ));
+    }
+    
+    /**
+     * @Route("/new-setting", name="new_setting")
+     */
+    public function newSettingAction(Request $request)
+    {
+        $setting = new Setting();
+        $setting->setAddress('my address');
+        $setting->setTimetable('my timetable');
+        $setting->setOrganizer('my organizer');
+        $setting->setCollaborator('my collaborator');
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($setting);
+        $em->flush();
+        
+        // replace this example code with whatever you need
+        return $this->render('default/new_settings.html.twig', array(
+            
         ));
     }
 }
