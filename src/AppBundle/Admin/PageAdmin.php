463334ec3fef7a9814e1aabdb30803ce3466ac26
Anton Serra <aserratorta@gmail.com>
2016-06-30 23:42:22 +0200

    improve PageAdmin

--- src/AppBundle/Admin/PageAdmin.php
+++ src/AppBundle/Admin/PageAdmin.php
@@ -6,7 +6,6 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 /**
@@ -44,11 +43,38 @@ class PageAdmin extends AbstractBaseAdmin
         $formMapper
             ->with('General', $this->getFormMdSuccessBoxArray(7))
             ->add(
+                'name',
+                null,
+                array(
+                    'label'    => 'Nom',
+                    'required' => false,
+                )
+            )
+            ->add(
                 'type',
                 null,
                 array(
                     'label'    => 'Tipus',
                     'required' => false,
+                )
+            )
+            ->add(
+                'place',
+                null,
+                array(
+                    'label'    => 'Lloc',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'summary',
+                TextareaType::class,
+                array(
+                    'label'    => 'Resum',
+                    'required' => false,
+                    'attr'     => array(
+                        'rows' => 6,
+                    )
                 )
             )
             ->add(
@@ -58,12 +84,152 @@ class PageAdmin extends AbstractBaseAdmin
                     'label'    => 'Descripció',
                     'required' => false,
                     'attr'     => array(
-                        'rows' => 8,
+                        'rows' => 6,
+                    )
+                )
+            )
+            ->add(
+                'links',
+                TextareaType::class,
+                array(
+                    'label'    => 'Links',
+                    'required' => false,
+                    'attr'     => array(
+                        'rows' => 6,
                     )
                 )
             )
             ->end()
             ->with('Controls', $this->getFormMdSuccessBoxArray(5))
+            ->add(
+                'publishDate',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'Data Publicació',
+                    'format' => 'd/M/y',
+                )
+            )
+            ->add(
+                'expirationDate',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'Data Caducitat',
+                    'format' => 'd/M/y',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'realizationDate',
+                null,
+                array(
+                    'label'    => 'Data Realització',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'video',
+                null,
+                array(
+                    'label'    => 'Video',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'urlVimeo',
+                null,
+                array(
+                    'required' => false,
+                )
+            )
+            ->add(
+                'urlFlickr',
+                null,
+                array(
+                    'required' => false,
+                )
+            )
+            ->add(
+                'smallImageName1',
+                null,
+                array(
+                    'label'    => 'Nom Imatge Petita 1',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'largeImageName',
+                null,
+                array(
+                    'label'    => 'Nom Imatge Gran',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'largeFooterImage',
+                null,
+                array(
+                    'label'    => 'Imatge Peu Gran',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'documentName1',
+                null,
+                array(
+                    'label'    => 'Nom Document 1',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'documentTitle1',
+                null,
+                array(
+                    'label'    => 'Títol Document 1',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'documentName2',
+                null,
+                array(
+                    'label'    => 'Nom Document 2',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'documentTitle2',
+                null,
+                array(
+                    'label'    => 'Títol Document 2',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'smallImageName2',
+                null,
+                array(
+                    'label'    => 'Nom Imatge Petita 2',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'startDate',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'Data Inicial',
+                    'format' => 'd/M/y',
+                    'required' => false,
+                )
+            )
+            ->add(
+                'endDate',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'Data Final',
+                    'format' => 'd/M/y',
+                    'required' => false,
+                )
+            )
             ->end();
         }
 
@@ -79,7 +245,82 @@ class PageAdmin extends AbstractBaseAdmin
                 array(
                     'label' => 'Name',
                 )
-            );
+            )
+            ->add(
+                'description',
+                null,
+                array(
+                    'label' => 'Descripció',
+                )
+            )
+            ->add(
+                'summary',
+                null,
+                array(
+                    'label' => 'Resum',
+                )
+            )
+            ->add(
+                'type',
+                null,
+                array(
+                    'label'    => 'Tipus',
+                )
+            )
+            ->add(
+                'publishDate',
+                'doctrine_orm_date',
+                array(
+                    'label'      => 'Data Publicació',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+            ->add(
+                'expirationDate',
+                'doctrine_orm_date',
+                array(
+                    'label'      => 'Data Caducitat',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+            ->add(
+                'realizationDate',
+                null,
+                array(
+                    'label'    => 'Data Realització',
+                )
+            )
+            ->add(
+                'place',
+                null,
+                array(
+                    'label'    => 'Lloc',
+                )
+            )
+            ->add(
+                'video',
+                null,
+                array(
+                    'label'    => 'Video',
+                )
+            )
+            ->add(
+                'startDate',
+                'doctrine_orm_date',
+                array(
+                    'label'      => 'Data Inicial',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+            ->add(
+                'endDate',
+                'doctrine_orm_date',
+                array(
+                    'label'      => 'Data Final',
+                    'field_type' => 'sonata_type_date_picker',
+                )
+            )
+        ;
     }
 
     /**
@@ -94,6 +335,31 @@ class PageAdmin extends AbstractBaseAdmin
                 null,
                 array(
                     'label'    => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'type',
+                null,
+                array(
+                    'label'    => 'Tipus',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'publishDate',
+                null,
+                array(
+                    'label'  => 'Data Publicació',
+                    'format' => 'd/m/Y'
+                )
+            )
+            ->add(
+                'place',
+                null,
+                array(
+                    'label'    => 'Lloc',
+                    'editable' => true,
                 )
             )
             ->add(
