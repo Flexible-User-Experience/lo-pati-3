e31b3b1d688248fb7fc99652df6b0124800853c3
Wil Iglesias <wiglesias83@gmail.com>
2016-07-01 18:41:47 +0200

    Create new Artist entity

--- src/AppBundle/Entity/Artist.php
+++ src/AppBundle/Entity/Artist.php
@@ -3,10 +3,10 @@
 namespace AppBundle\Entity;
 
 use AppBundle\Entity\Traits\DescriptionTrait;
+use AppBundle\Entity\Traits\Document1Trait;
 use AppBundle\Entity\Traits\Image1Trait;
-use AppBundle\Entity\Traits\LinkTrait;
 use AppBundle\Entity\Traits\NameTrait;
-use AppBundle\Entity\Traits\PositionTrait;
+use AppBundle\Entity\Traits\SummaryTrait;
 use AppBundle\Entity\Traits\YearTrait;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -24,10 +24,71 @@ class Artist extends AbstractBase
 {
     use NameTrait;
     use YearTrait;
+    use SummaryTrait;
     use DescriptionTrait;
     use Image1Trait;
     use Image2Trait;
-    
+    use Document1Trait;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $city;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $category;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+    
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * 
+     * @return Artist
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param string $category
+     * 
+     * @return Artist
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+        
+        return $this;
+    }
 }
