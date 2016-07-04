7795379742fb61a50701da8aaa20526b90bcd76a
Wil Iglesias <wiglesias83@gmail.com>
2016-07-04 16:56:29 +0200

    Refactor Document1Trait

--- src/AppBundle/Entity/Traits/Document1Trait.php
+++ src/AppBundle/Entity/Traits/Document1Trait.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * Document1 trait
@@ -18,23 +21,59 @@ Trait Document1Trait
      * @var string
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public $document1;
+    public $document1Name;
+
+    /**
+     *
+     *
+     * Methods
+     *
+     *
+     */
+
+    /**
+     * @return File|UploadedFile
+     */
+    public function getDocument1File()
+    {
+        return $this->document1File;
+    }
+
+    /**
+     * @param File|UploadedFile $document1File
+     *
+     * @return $this
+     */
+    public function setDocument1File(File $document1File = null)
+    {
+        $this->document1File = $document1File;
+        if ($document1File) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
 
     /**
      * @return string
      */
-    public function getDocument1()
+    public function getDocument1Name()
     {
-        return $this->document1;
+        return $this->document1Name;
     }
 
     /**
-     * @param string $document1
+     * @param string $document1Name
+     * 
      * @return Document1Trait
      */
-    public function setDocument1($document1)
+    public function setDocument1Name($document1Name)
     {
-        $this->document1 = $document1;
+        $this->document1Name = $document1Name;
+        
         return $this;
     }
 }
