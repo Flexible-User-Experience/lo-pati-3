721d216aa3f33be6d1ab84945002ba53d3d87562
Wil Iglesias <wiglesias83@gmail.com>
2016-07-06 17:38:40 +0200

    Add MenuLevel1 Admin List View

---
+++ src/AppBundle/Admin/MenuLevel1Admin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\CoreBundle\Form\Type\DatePickerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+/**
+ * Class MenuLevel1Admin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Wilson Iglesias <wiglesias83@gmail.com>
+ */
+class MenuLevel1Admin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Categoria';
+    protected $baseRoutePattern = 'pages/menuLevel1';
+    protected $datagridValues = array(
+        '_sort_by'    => 'position',
+        '_sort_order' => 'asc',
+    );
+    
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'position',
+                null,
+                array(
+                    'label'    => 'Posició',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'name',
+                null,
+                array(
+                    'label'    => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'isArchive',
+                null,
+                array(
+                    'label'    => 'Arxiu',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'Accions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
