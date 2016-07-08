6fe8b88aac18122a844450bc713ec590f4623954
Wil Iglesias <wiglesias83@gmail.com>
2016-07-08 18:44:59 +0200

    Add MenuLevel2 Admin List View

---
+++ src/AppBundle/Admin/MenuLevel2Admin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\CoreBundle\Form\Type\DatePickerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+/**
+ * Class MenuLevel2Admin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Wilson Iglesias <wiglesias83@gmail.com>
+ */
+class MenuLevel2Admin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Subcategoria';
+    protected $baseRoutePattern = 'pages/menuLevel2';
+    protected $datagridValues = array(
+        '_sort_by'    => 'position',
+        '_sort_order' => 'asc',
+    );
+    
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'position',
+                null,
+                array(
+                    'label'    => 'Posició',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'name',
+                null,
+                array(
+                    'label'    => 'Nom',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'isList',
+                null,
+                array(
+                    'label'    => 'Llista',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'Actiu',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label'   => 'Accions',
+                    'actions' => array(
+                        'edit'   => array('template' => '::Admin/Buttons/list__action_edit_button.html.twig'),
+                    )
+                )
+            );
+    }
+}
